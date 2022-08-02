c9f60beb24d0c444d31feb8a7bbf12071f0209b1
Esra-Turk <esra.turk@bil.omu.edu.tr>
2022-08-03 01:44:09 +0300

    üçgen görevi tamamlandı

---
+++ Temel-Kavramlar/Ucgende-Hesaplamalar/README.md
@@ -0,0 +1,18 @@
+# Bir Üçgende Hipotenüs Uzunluğu ve Alan Hesaplama
+
+Java ile kullanıcıdan üçgenin dik kenarlarının uzunluğunu alan ve hipotenüsü hesaplayan program
+
+Hipotenüs uzunluğu bulma formulü 
+
+`a^2 = b^2 + c^2`
+
+Üç kenar uzunluğu kullanıcıdan alınan üçgenin alanını hesaplamak için formül
+
+```
+Üç𝑔𝑒𝑛𝑖𝑛 ç𝑒𝑣𝑟𝑒𝑠𝑖 = 2𝑢
+
+𝑢 = (a+b+c) / 2
+
+Alan * Alan = 𝑢 * (𝑢 − 𝑎)* (𝑢 − 𝑏) * (𝑢 − 𝑐)
+
+```
---
+++ Temel-Kavramlar/Ucgende-Hesaplamalar/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		//Hipotenüs uzunluğunu bulma
+		
+		double a, b, c; //dik üçgenin kenarları
+		
+		Scanner kenar = new Scanner(System.in);
+		
+		System.out.print("1.dik kenar: ");
+		a = kenar.nextDouble();
+		System.out.print("2.dik kenar: ");
+		b = kenar.nextDouble();
+		
+		c = Math.sqrt(a*a + b*b); //hipotenüs dik kenarların kareleri toplamının kareköküne eşittir.Math.sqrt karekök alma fonksiyonudur
+		System.out.println("Hipotenüs uzunluğu: "+c);
+		
+		//3 kenarı verilen üçgenin alanını bulma
+		
+		double u, cevre;
+		cevre = a + b + c;
+		u = cevre / 2;
+		
+		double alan = Math.sqrt(u * (u-a) * (u-b) * (u-c)); //üçgen alan hesabı
+		System.out.println("Üçgenin alanı: " +alan);
+		
+				
+		
+	}
+
+}
