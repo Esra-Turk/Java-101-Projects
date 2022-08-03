aec3af990d40e829ae0cbc00fdd1eae52845e869
Esra-Turk <esra.turk@bil.omu.edu.tr>
2022-08-03 12:37:41 +0300

    daire görevi tamamlandi

---
+++ Temel-Kavramlar/Daire/README.md
@@ -0,0 +1,17 @@
+# Dairenin Alanını ve Çevresini Hesaplayan Program
+
+Java ile yarı çapını kullanıcıdan aldığınız dairenin alanını ve çevresini hesaplayan program
+
+Alan Formülü
+
+`π * r * r`
+
+Çevre Formülü
+
+`2 * π * r`
+
+𝜋 sayısı = 3.14
+
+Yarıçapı r, merkez açısının ölçüsü 𝛼 olan daire diliminin alanı bulan program için formül
+
+`(𝜋 * (r*r) * 𝛼) / 360`
---
+++ Temel-Kavramlar/Daire/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		float PI = 3.14f;
+		float cevre, alan;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Dairenin yarıçapı: " );
+		double yaricap = input.nextDouble();
+		
+		System.out.print("Dairenin merkez açıcı ölçüsü: ");
+		int aci = input.nextInt();
+		
+		cevre = (float) (2 * PI * yaricap); //dairenin çevresini hesaplama
+		alan = (float) (PI * (yaricap * yaricap ) * aci) / 360; //dairenin alanını hesaplama
+		
+		System.out.println("Dairenin çevresi: " +cevre);
+		System.out.println("Dairenin alanı: " +alan);
+
+		
+		
+		
+		
+		
+		
+	}
+
+}
