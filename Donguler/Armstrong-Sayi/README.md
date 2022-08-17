e1eda14911007ba02f8556a9faefd90868f5c108
Esra-Turk <esra.turk@bil.omu.edu.tr>
2022-08-17 03:02:08 +0300

    görev tamamlandı

---
+++ Donguler/Armstrong-Sayi/README.md
@@ -0,0 +1,28 @@
+# Armstrong Sayı
+
+N haneli bir sayının basamaklarının n’inci üstlerinin toplamı, kendisine eşitse bu sayıya Armstrong sayı denir.
+
+Örneğin 407 sayısını ele alalım. (4^3)+ (0^3)+(7^3) = 64+0+343 = 407 sonucunu verir. Bu da 407 sayısının armstrong bir sayı olduğunu gösterir.
+
+1342 -> (1^4)+(3^4)+(4^4)+(2^4) =1+81+256+16=354 sayısı 1342’ye eşit olmadığı için armstrong sayı değildir.
+
+1634=1^4+6^4+3^4+4^4=1+1296+81+256=1634 Armstrong sayıdır
+
+54748=5^5+4^5+7^5+4^5+8^5=3125+1024+16807+1024+32768=54748 Armstrong sayıdır
+
+**Senaryo**
+
+```
+Sayı giriniz: 1634
+1634 Armstrong sayıdır
+
+```
+
+```
+Sayı giriniz: 1203
+1203 Armstrong sayı değildir
+
+```
+
+## SOURCE
+[Patika.dev](https://www.patika.dev/tr)
---
+++ Donguler/Armstrong-Sayi/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int number;
+		int digitNumber = 0; //basamak sayısı
+		int result = 0; //toplama işlemi için değişken
+	
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Sayı giriniz: ");
+		number = input.nextInt();
+		
+		int tempNumber = number; //sayının ilk değerinin korumak için geçici değişken oluşturuldu
+		while(tempNumber != 0) {
+			tempNumber /= 10;
+			digitNumber++;	
+		}
+		
+		tempNumber = number;
+		while(tempNumber != 0) {
+			int value = tempNumber % 10; //sayının birler basamağındaki rakamı hesaplar.
+			result += Math.pow(value, digitNumber);
+			tempNumber /= 10;	
+		}
+		
+		if(result == number) {
+			System.out.println(number+ " Armstrong sayıdır");
+		}
+		else {
+			System.out.println(number+ " Armstrong sayı değildir");
+		}
+			
+	}
+
+}
