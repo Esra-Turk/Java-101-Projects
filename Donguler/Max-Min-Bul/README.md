865e2a4a06676c0e965cd0c7b254437d10447535
Esra-Turk <esra.turk@bil.omu.edu.tr>
2022-08-20 23:11:10 +0300

    Max-Min bulma tamamlandı

---
+++ Donguler/Max-Min-Bul/README.md
@@ -0,0 +1,25 @@
+# Girilen Sayılardan Min ve Max Değerleri Bulma
+
+Java ile klavyeden girilen N tane sayma sayısından en büyük ve en küçük sayıları bulan ve bu sayıları ekrana yazan program
+
+**Senaryo**
+
+```
+Kaç tane sayı gireceksiniz: 10
+1. Sayıyı giriniz: 5
+2. Sayıyı giriniz: 96
+3. Sayıyı giriniz: 65
+4. Sayıyı giriniz: 87
+5. Sayıyı giriniz: 236
+6. Sayıyı giriniz: -5
+7. Sayıyı giriniz: 4
+8. Sayıyı giriniz: 1
+9. Sayıyı giriniz: 2
+10. Sayıyı giriniz: 3
+Dizideki en büyük değer: 236
+Dizideki en küçük değer: -5
+
+```
+
+##SOURCE
+[Patika.dev](https://www.patika.dev/tr)
---
+++ Donguler/Max-Min-Bul/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int howMany;
+		int number;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Kaç tane sayı gireceksiniz: ");
+		howMany = input.nextInt();
+		
+		int index = 0;
+		int dizi[] = new int[howMany];
+		
+		for(int i = 1; i <= howMany; i++) {
+			System.out.print(i+ ". Sayıyı giriniz: ");
+			number = input.nextInt();
+			
+			dizi[index] = number;
+			index++;
+		}
+		
+        int large = dizi[0];
+        int least = dizi[0];
+		
+        for (int i = 0; i < dizi.length; i++){
+            if (dizi[i] >large) 
+                large = dizi[i];
+            
+            if(dizi[i] < least)
+            	least = dizi[i];   
+        }
+        System.out.println("Dizideki en büyük değer: " + large);
+        System.out.println("Dizideki en küçük değer: " + least);
+	}
+}
