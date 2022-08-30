5fe35c4c8b67a6ca5d036c46c4d6bfce98cab3aa
Esra-Turk <esra.turk@bil.omu.edu.tr>
2022-08-30 15:01:24 +0300

    Fibonacci serisi tamamlandı

---
+++ Metotlar/Fibonacci-Serisi/README.md
@@ -0,0 +1,21 @@
+# Fibonacci Serisi
+
+Recursive fonksiyon kullanarak kullanıcıdan alınan index değeriyle serinin o indexteki elemanını ekrana yazdıran program
+
+**Senaryolar**
+
+```
+Fibonacci serisinin kaçıncı indisi:6
+8
+
+```
+
+```
+Fibonacci serisinin kaçıncı indexi:2
+1
+```
+
+## Source
+
+[Patika.dev](https://www.patika.dev/tr)
+
---
+++ Metotlar/Fibonacci-Serisi/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+	static int fibonacci(int number) {
+		if(number <= 1) 
+			return number;
+		
+		return fibonacci(number-1) + fibonacci(number-2);
+		
+	}
+	
+	public static void main(String[] args) {
+		int number;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Fibonacci serisinin kaçıncı indexi:");
+		number = input.nextInt();
+		
+		System.out.println(fibonacci(number));
+	}
+}
