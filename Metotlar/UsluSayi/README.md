b445d0ef403e2bf8e3ef20f62b8bc25fc64f4194
Esra-Turk <esra.turk@bil.omu.edu.tr>
2022-08-30 19:50:14 +0300

    Üslü sayı tamamlandı

---
+++ Metotlar/UsluSayi/README.md
@@ -0,0 +1,28 @@
+# Recursive Metot ile Üslü Sayı Hesaplama
+
+Java'da taban ve üs değerleri kullanıcıdan alınan üs alma işlemini Recursive Metot kullanarak yapan program
+
+**Senaryo**
+
+```
+Üslü Sayı Hesaplama Programı
+Programdan çıkmak istediğinizde 0 giriniz.
+**********************************************
+Taban değeri giriniz: 2
+Üs değerini giriniz: 3
+Sonuç: 8
+Taban değeri giriniz: 5
+Üs değerini giriniz: 1
+Sonuç: 5
+Taban değeri giriniz: 1
+Üs değerini giriniz: 0
+Sonuç: 1
+Taban değeri giriniz: 0
+Programdan çıkılıyor..
+
+
+```
+
+## SOURCE
+
+[Patika.dev](https://www.patika.dev/tr)
---
+++ Metotlar/UsluSayi/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	static int power(int taban,int us) {
+		//üs 1 olduğunda sonuç taban sayıdır
+		if(us == 1) 
+			return taban;
+		
+		//üs sıfırsa sonuç 1'dir
+		if(us == 0)
+			return 1;
+	
+		return power(taban, us -1) * taban;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Üslü Sayı Hesaplama Programı\nProgramdan çıkmak istediğinizde 0 giriniz.\n**********************************************");
+		
+		while(true) {
+			Scanner input = new Scanner(System.in);
+			
+			System.out.print("Taban değeri giriniz: ");
+			int num1 = input.nextInt();
+			if(num1 == 0) {
+				System.out.println("Programdan çıkılıyor..");
+				System.exit(0);
+			}
+			
+			System.out.print("Üs değerini giriniz: ");
+			int num2 = input.nextInt();
+			
+			int result = power(num1,num2);
+			System.out.println("Sonuç: " +result);
+			
+		}
+	}
+}
