68584f2584119b82c66e1191fc76415ec6885d93
Esra-Turk <esra.turk@bil.omu.edu.tr>
2022-09-05 01:34:18 +0300

    Harmonik Seri tamamlandı

---
+++ Diziler/HarmonikSeri/README.md
@@ -0,0 +1,13 @@
+# Harmonik Seri Ortalaması
+
+Java diziler ile girilen sayının harmonik seri ortalamasını bulan program
+
+Harmonik Ortalama formülü : n (eleman sayısı) / elemanların harmonik serisi
+
+Harmonik Seri Formülü :
+
+1 + (1/2) + (1/3) + (1/4) +.....+(1/n)
+
+## Source
+
+[Patika.dev](https://www.patika.dev/tr)
---
+++ Diziler/HarmonikSeri/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+	 public static void main(String[] args) {
+		 int[] numbers = {1, 2, 3, 4, 5};
+		 
+		 double sum = 0.0;
+	     for (int i = 0; i < numbers.length; i++) {
+	    	 sum += (1.0 / numbers[i]);
+	     }
+	     
+	     System.out.println("Harmonik seri ortalaması:" + numbers.length / sum);
+	 }
+}
