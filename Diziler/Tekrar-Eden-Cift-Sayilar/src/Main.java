9edcaf55b0b8350f591ee3d4e41cd3c671593399
Esra Türk <74728647+Esra-Turk@users.noreply.github.com>
2022-09-06 22:32:34 +0300

    Update Main.java

--- Diziler/Tekrar-Eden-Cift-Sayilar/src/Main.java
+++ Diziler/Tekrar-Eden-Cift-Sayilar/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		Arrays.sort(arr);
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = i + 1; j < arr.length; j++) {
-				if(arr[i] == arr[j] && (arr[j] % 2 == 0)){
+				if(arr[i] == arr[j] && arr[j] % 2 == 0){
 					System.out.print(arr[j] + " ");
 				}
 			}
