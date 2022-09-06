be17e399680be002efcb1fdf662ef59918e858cd
Esra-Turk <esra.turk@bil.omu.edu.tr>
2022-09-06 18:37:50 +0300

    Yakın sayıları bulan program tamamlandı

---
+++ Diziler/Max-Min-Deger/README.md
@@ -0,0 +1,25 @@
+# Büyük ve Küçük Sayıyı Bulan Program
+
+Dizideki elemanların girilen sayıdan küçük en yakın sayıyı ve en büyük en yakın sayıyı bulan program
+
+**Senaryolar**
+
+```
+Dizi:[15, 12, 788, 1, -1, -778, 2, 0]
+Sayı giriniz:5
+Girilen sayıdan küçük en yakın sayı: 2
+Girilen sayıdan büyük en yakın sayı: 12
+
+```
+
+```
+Dizi:[15, 12, 788, 1, -1, -778, 2, 0]
+Sayı giriniz:100
+Girilen sayıdan küçük en yakın sayı: 15
+Girilen sayıdan büyük en yakın sayı: 788
+
+```
+
+## Source
+
+[Patika.dev](https://www.patika.dev/tr)
---
+++ Diziler/Max-Min-Deger/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		int number;
+		int[] array = {15,12,788,1,-1,-778,2,0};
+		System.out.println("Dizi:" +Arrays.toString(array));
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Sayı giriniz:");
+		number = input.nextInt();
+		
+		//önce dizi sıralanır
+		Arrays.sort(array);
+		
+		for(int i = 0; i <= array.length; i++) {
+			if(array[i] > number) {
+				System.out.println("Girilen sayıdan küçük en yakın sayı: " + array[i-1]);
+				break;
+			}
+		}
+		
+		for(int j = 0; j <= array.length; j++) {
+			if(array[j] > number) {
+				System.out.println("Girilen sayıdan büyük en yakın sayı: " + array[j]);
+				break;	
+			}
+		}	
+	}
+}
