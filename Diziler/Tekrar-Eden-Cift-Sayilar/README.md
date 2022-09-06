ec0a4bd33d516e976182925171252f18def5664f
Esra-Turk <esra.turk@bil.omu.edu.tr>
2022-09-06 22:28:43 +0300

    Çift Sayilar görevi tamamlandı

---
+++ Diziler/Tekrar-Eden-Cift-Sayilar/README.md
@@ -0,0 +1,14 @@
+#  Bir sayı dizisindeki tekrar eden çift sayıları bulan program
+
+Örneğin: {0,5,0,9,3,4,7,8,5,2,6,4,2,9,8,5,3,1,7,6,3} dizisi için
+
+**Senaryo**
+
+```
+En çok tekrar eden sayilar: 0 2 4 6 8 
+
+```
+
+## Source
+
+[Patika.dev](https://www.patika.dev/tr)
---
+++ Diziler/Tekrar-Eden-Cift-Sayilar/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Main {
+	public static void isPairArray(int [] arr){
+		System.out.print("En çok tekrar eden sayilar: ");
+		
+		Arrays.sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if(arr[i] == arr[j] && (arr[j] % 2 == 0)){
+					System.out.print(arr[j] + " ");
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int [] list = {0,5,0,9,3,4,7,8,5,2,6,4,2,9,8,5,3,1,7,6,3};
+		isPairArray(list);
+	}
+}
