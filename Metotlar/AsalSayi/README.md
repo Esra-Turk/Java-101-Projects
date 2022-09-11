6190e3c50089b7ae203cc6193844795de86d65d8
Esra-Turk <esra.turk@bil.omu.edu.tr>
2022-09-11 22:53:04 +0300

    Asal Sayı tamamlandı

---
+++ Metotlar/AsalSayi/README.md
@@ -0,0 +1,20 @@
+# Asal Sayı
+
+Recursive metot kullanarak  kullanıcıdan alınan sayının asal sayı olup olmadığını bulan program
+
+**Senaryo**
+
+```
+Sayi giriniz: 19
+19 asal sayidir.
+```
+
+```
+Sayi giriniz: 1
+1 asal sayi degildir.
+
+```
+
+## Source
+
+[Patika.dev](https://www.patika.dev/tr)
--- Metotlar/AsalSayi/src/Main.java
+++ Metotlar/AsalSayi/src/Main.java
@@ -1,17 +1,36 @@
 import java.util.Scanner;
 
 public class Main {
-	static void isPrime(int num) {
-		
-		
-	}
-	public static void main(String[] args) {
-		int number;
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Sayı giriniz: ");
-		number = scan.nextInt();
-		
-		isPrime(number);
-	}
+    static boolean isPrime(int num, int diviser) {
+        if(num < 2) {
+            return false;
+        }
 
+        if(diviser == 1) {
+            return true;
+        }
+
+        if(num % diviser == 0) {
+            return false;
+        }
+
+        return isPrime(num, diviser-1);
+    }
+
+    public static void main(String[] args) {
+        int number;
+        boolean primeNumber;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Sayi giriniz: ");
+        number = scan.nextInt();
+
+        primeNumber = isPrime(number, number/2);
+        if(primeNumber == true) {
+            System.out.println(number + " asal sayidir.");
+        }
+        else {
+            System.out.println(number + " asal sayi degildir.");
+        }
+    }
 }
