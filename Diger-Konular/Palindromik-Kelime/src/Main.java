af393fd5eae13c3a31f601155698d5b9fdca4a24
Esra-Turk <esra.turk@bil.omu.edu.tr>
2022-09-14 17:19:15 +0300

    Palindromik kelimeler tamamlandı

---
+++ Diger-Konular/Palindromik-Kelime/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+	public static boolean isPalindrom(String str) {
+        String reverse = "";
+        //Kullanıcının girdiği büyük harfler üçük harfe dönüştürülür.
+        String word = str.toLowerCase();
+        
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reverse += word.charAt(i);
+        }
+
+        return word.equals(reverse);
+    }
+		
+	public static void main(String[] args) {
+		String word;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Kelime giriniz: ");
+		word = input.nextLine();
+		
+		if(isPalindrom(word) == true) {
+			System.out.println(word + " Palindromik bir kelimedir.");
+		}
+		else {
+			System.out.println(word + " Palindromik bir kelime değildir.");
+		}
+	}
+}
